b6dee3978b4d1f4c74d27b0fa0f1f6493a028a01
Jonatan Alves <alveslima.ti@hotmail.com>
2025-04-10 23:23:31 -0300

    process change port server

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const WebSocket = require('ws');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3002;
 
 // Criar o Servidor WebSocket
 const wss = new WebSocket.Server({ noServer: true });
